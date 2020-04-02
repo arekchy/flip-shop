a6e4337879453b56c7206552b850b7be3549e535
Arek <achybinski@gmail.com>
2020-04-02 20:50:01 +0200

    shopping cart controller tests coverage

--- src/shopping-cart/shopping-cart.controller.spec.ts
+++ src/shopping-cart/shopping-cart.controller.spec.ts
@@ -2,6 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ShoppingCartController } from './shopping-cart.controller';
 import { ShoppingCartService } from './shopping-cart.service';
 import { ProductsService } from '../products/products.service';
+import { ShoppingCartDto } from './interfaces/shopping-cart.dto';
+import { ShoppingCartAction } from './interfaces/shopping-cart.action';
+import { OrderCheckoutDto } from './interfaces/order-checkout.dto';
 
 describe('ShoppingCart Controller', () => {
   let controller: ShoppingCartController;
@@ -9,10 +12,13 @@ describe('ShoppingCart Controller', () => {
 
   };
   const shoppingCartService = {
-
+    create: (): string => { return; },
+    update: (): ShoppingCartDto => { return; },
+    checkout: (): OrderCheckoutDto => { return; },
   };
 
   beforeEach(async () => {
+    jest.resetAllMocks();
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ShoppingCartController],
       providers: [
@@ -32,5 +38,38 @@ describe('ShoppingCart Controller', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should return cart id', () => {
+      const cartId = 'cartId';
+      jest.spyOn(shoppingCartService, 'create').mockReturnValueOnce(cartId);
+
+      expect(controller.create()).toBe(cartId);
+    });
+  });
+
+  describe('update', () => {
+    it('should return shopping cart', () => {
+      const cartId = 'cartId';
+      const shoppingCart = new ShoppingCartDto();
+      jest.spyOn(shoppingCartService, 'update').mockReturnValueOnce(shoppingCart);
+
+      expect(controller.update(cartId, {
+        action: ShoppingCartAction.REMOVE,
+        productId: 'pid',
+        quantity: 10,
+      })).toBe(shoppingCart);
+    });
+  });
+
+  describe('checkout', () => {
+    it('should return order checkout', () => {
+      const cartId = 'cartId';
+      const orderCheckout = {} as OrderCheckoutDto;
+      jest.spyOn(shoppingCartService, 'checkout').mockReturnValueOnce(orderCheckout);
+
+      expect(controller.checkout(cartId, 'EUR')).toBe(orderCheckout);
+    });
   });
 });
