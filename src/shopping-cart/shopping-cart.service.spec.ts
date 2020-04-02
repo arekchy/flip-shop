47947da9aa0e0efb9639ba263580413e42b0caab
Arek <achybinski@gmail.com>
2020-04-02 20:16:05 +0200

    shopping cart service - checkout tests

--- src/shopping-cart/shopping-cart.service.spec.ts
+++ src/shopping-cart/shopping-cart.service.spec.ts
@@ -296,5 +296,204 @@ describe('ShoppingCartService', () => {
 
     });
 
+    describe.only('checkout', () => {
+      it('should return list of products with calculated price in selected currency (basic currency, without exchange)', async () => {
+        const product1 = products[0];
+        jest
+          .spyOn(productsService, 'findById')
+          .mockReturnValue(product1);
+
+        const requestedCurrency = 'EUR';
+        const conversionValue = 15;
+        jest
+          .spyOn(currenciesService, 'convert')
+          .mockReturnValue(conversionValue);
+
+        const cartId = service.create();
+        const update1Input = {
+          quantity: 3,
+          productId: product1.id,
+          action: ShoppingCartAction.ADD,
+        };
+        service.update(cartId, update1Input);
+
+        const result = await service.checkout(cartId);
+        expect(result).toEqual({
+          shoppingCartId: expect.any(String),
+          detailedProductsList: [
+            {
+              name: product1.name,
+              quantity: update1Input.quantity,
+              description: product1.description,
+              originalPrice: {
+                currency: product1.price.currency,
+                value: product1.price.value,
+                total: product1.price.value * update1Input.quantity,
+              },
+              requestedCurrencyPrice: {
+                currency: requestedCurrency,
+                value: product1.price.value,
+                total: product1.price.value * update1Input.quantity,
+              },
+            }
+          ],
+          totalPrice: { value: conversionValue * update1Input.quantity, currency: requestedCurrency }
+        });
+
+        expect(currenciesService.convert).not.toBeCalled();
+      });
+
+      it('should return list of products with calculated price in selected currency', async () => {
+        const product1 = products[0];
+        jest
+          .spyOn(productsService, 'findById')
+          .mockReturnValue(product1);
+
+        const requestedCurrency = 'PLN';
+        const conversionValue = 10;
+        jest
+          .spyOn(currenciesService, 'convert')
+          .mockReturnValue(conversionValue);
+
+        const cartId = service.create();
+        const update1Input = {
+          quantity: 3,
+          productId: product1.id,
+          action: ShoppingCartAction.ADD,
+        };
+        service.update(cartId, update1Input);
+
+        const result = await service.checkout(cartId, 'PLN');
+        expect(result).toEqual({
+          shoppingCartId: expect.any(String),
+          detailedProductsList: [
+            {
+              name: product1.name,
+              quantity: update1Input.quantity,
+              description: product1.description,
+              originalPrice: {
+                currency: product1.price.currency,
+                value: product1.price.value,
+                total: product1.price.value * update1Input.quantity,
+              },
+              requestedCurrencyPrice: {
+                currency: requestedCurrency,
+                value: conversionValue,
+                total: conversionValue * update1Input.quantity,
+              },
+            }
+          ],
+          totalPrice: { value: conversionValue * update1Input.quantity, currency: requestedCurrency }
+        });
+
+        expect(currenciesService.convert).toBeCalledTimes(1);
+      });
+
+      it('should return list of products with calculated price in selected currency', async () => {
+        const product1 = products[0];
+        const product2 = products[1];
+        jest
+          .spyOn(productsService, 'findById')
+          .mockReturnValueOnce(product1)
+          .mockReturnValueOnce(product2)
+          .mockReturnValueOnce(product1)
+          .mockReturnValueOnce(product2);
+
+        const conversionValue = 10;
+        jest
+          .spyOn(currenciesService, 'convert')
+          .mockReturnValue(conversionValue);
+
+        const requestedCurrency = 'GBP';
+
+        const cartId = service.create();
+        const update1Input = {
+          quantity: 3,
+          productId: product1.id,
+          action: ShoppingCartAction.ADD,
+        };
+        service.update(cartId, update1Input);
+
+        const update2Input = {
+          quantity: 1,
+          productId: product2.id,
+          action: ShoppingCartAction.ADD,
+        };
+        service.update(cartId, update2Input);
+
+        const result = await service.checkout(cartId, requestedCurrency);
+
+        expect(result).toEqual({
+          shoppingCartId: expect.any(String),
+          detailedProductsList: [
+            {
+              name: product1.name,
+              quantity: update1Input.quantity,
+              description: product1.description,
+              originalPrice: {
+                currency: product1.price.currency,
+                value: product1.price.value,
+                total: product1.price.value * update1Input.quantity,
+              },
+              requestedCurrencyPrice: {
+                currency: requestedCurrency,
+                value: conversionValue,
+                total: update1Input.quantity * conversionValue,
+              },
+            },
+            {
+              name: product2.name,
+              quantity: update2Input.quantity,
+              description: product2.description,
+              originalPrice: {
+                currency: product2.price.currency,
+                value: product2.price.value,
+                total: product2.price.value * update2Input.quantity,
+              },
+              requestedCurrencyPrice: {
+                currency: requestedCurrency,
+                value: conversionValue,
+                total: conversionValue * update2Input.quantity,
+              },
+            }
+          ],
+          totalPrice: { value: conversionValue * (update2Input.quantity + update1Input.quantity), currency: requestedCurrency }
+        });
+
+        expect(currenciesService.convert).toBeCalledTimes(2);
+      });
+
+      it('When requested more products than available, should return UnprocessableEntityException', async () => {
+        const product1 = products[0];
+        jest
+          .spyOn(productsService, 'findById')
+          .mockReturnValueOnce(product1)
+          .mockReturnValueOnce({
+            ...product1,
+            quantity: 0,
+          });
+
+        const requestedCurrency = 'PLN';
+        const conversionValue = 10;
+        jest
+          .spyOn(currenciesService, 'convert')
+          .mockReturnValue(conversionValue);
+
+        const cartId = service.create();
+        const update1Input = {
+          quantity: 3,
+          productId: product1.id,
+          action: ShoppingCartAction.ADD,
+        };
+        service.update(cartId, update1Input);
+
+        expect(service.checkout(cartId, 'PLN'))
+          .rejects.toEqual(new UnprocessableEntityException(`Insufficient products quantity in warehouse: ${product1.name}`));
+
+        expect(currenciesService.convert).toBeCalledTimes(1);
+      });
+
+    });
+
   });
 });
